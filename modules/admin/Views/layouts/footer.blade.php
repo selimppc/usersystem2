0d3cdfa4e5d41456cd6e8b2565cbc5eba77eb1dd
Almas Hossain <almas@edutechsolutionsbd.com>
2016-07-17 17:15:53 +0600

    Copyright text change.

--- modules/admin/Views/layouts/footer.blade.php
+++ modules/admin/Views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
         <h6 class="text-center">
-            COPYRIGHT © <?php echo date('Y'); ?> · ALL RIGHTS RESERVED SIB Signs &amp; Designs
+            COPYRIGHT © <?php echo date('Y'); ?> · ALL RIGHTS RESERVED BY EDU TECH SOLUTIONS
         </h6>
 
 </footer>
