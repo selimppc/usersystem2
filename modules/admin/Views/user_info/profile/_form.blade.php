09e7dd725c9ddf2ace4e9831a8e470e0121027f6
Shawon <bd.shawon1991@gmail.com>
2016-07-19 14:09:05 +0600

    updated

--- modules/admin/Views/user_info/profile/_form.blade.php
+++ modules/admin/Views/user_info/profile/_form.blade.php
@@ -1,4 +1,3 @@
-<script src="assets/bitd/js/jquery.min.js"></script>
 <div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
     {!! Form::hidden('user_id',$user_id) !!}
     <div class="row">
@@ -69,82 +68,4 @@
     <a href="{{route('user-profile')}}" class=" btn btn-default" data-placement="top" data-content="click close button for close this entry form">Close</a>
 </div>
 
-
-<script>
-
-    //document.onload = function(e) {
-    $(function () {
-        $("#form_2").validate({
-            rules: {
-                name: {
-                    required: true,
-                },
-                password: {
-                    required: true,
-                },
-                url: {
-                    required: true,
-                    url: true
-                },
-                number: {
-                    required: true,
-                    number: true
-                },
-                max: {
-                    required: true,
-                    maxlength: 4
-                }
-            },
-            submitHandler: function (form) {
-                form.submit();
-            }
-        });
-
-        $("#form_2").validate({
-            rules: {
-                name: {
-                    required: true,
-                },
-                username: {
-                    required: true,
-                },
-                url: {
-                    required: true,
-                    url: true
-                },
-                number: {
-                    required: true,
-                    number: true
-                },
-                last_name: {
-                    required: true,
-                    minlength: 6
-                }
-            },
-            messages: {
-                number: {
-                    required: "(Please enter your phone number)",
-                    number: "(Please enter valid phone number)"
-                },
-                last_name: {
-                    required: "This is custom message for required",
-                    minlength: "This is custom message for min length"
-                }
-            },
-            submitHandler: function (form) {
-                form.submit();
-            },
-            errorPlacement: function (error, element) {
-                $(element)
-                        .closest("form")
-                        .find("label[for='" + element.attr("id") + "']")
-                        .append(error);
-            },
-            errorElement: "span",
-        });
-    });
-    //}
-</script>
-
-@include('admin::user_info.profile._script')
 
